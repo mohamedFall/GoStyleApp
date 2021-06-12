8d159e0e7575183b68f782d83eab2d2ba4a1b368
mohamedFall <mohamed.fall@epsi.fr>
2021-06-12 13:50:21 +0200

    last commit

--- App/src/screens/ProfileScreen.js
+++ App/src/screens/ProfileScreen.js
@@ -1,10 +1,95 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useContext } from 'react'
+import { View } from 'react-native'
+
+import Button from '../components/Button'
+
+import styled from 'styled-components/native'
+import Text from '../components/Text'
+
+import { theme } from '../core/theme'
+
+import { MainContext } from '../context/MainContext'
+import { UserContext } from '../context/UserContext'
 
 export default ProfileScreen = () => {
+    const [user, setUser] = useContext(UserContext)
+    const [isloading, setIsLoading] = useState(true)
+
+    const mainContext = useContext(MainContext)
+
+    const logOut = async () => {
+        const loggedOut = await mainContext.logOut()
+        console.log(loggedOut)
+    
+        if(loggedOut) {
+            setUser(state => ({
+              uid: null,
+              firstName: "",
+              lastName: "",
+              email: "",
+              username: "",
+              myItems: null,
+              isLoggedIn: false,
+            }))
+        }
+    }
+
     return (
-        <View>
-            <Text>ProfileScreen</Text>
-        </View>
+        <Container>
+            <Header>
+                <Text medium heavy color={theme.colors.primary}>Profile</Text>
+            </Header>
+
+            <Main>
+                <AvatarContainer>
+                    <Avatar source={require('../assets/avatardefault.png')} />
+                    <Text large heavy margin="24px 0 0 0">{user.firstName + ' ' + user.lastName}</Text>
+                    <Text medium semi>{user.username}</Text>
+                </AvatarContainer>
+                <ButtonsContainer>
+                    <Button
+                        mode="outlined"
+                        onPress={logOut} >
+                        Logout
+                    </Button>
+                </ButtonsContainer>
+            </Main>
+        </Container>
     )
 }
+
+const Container = styled.View`
+    flex: 1;
+`
+
+const Header = styled.View`
+    position: absolute;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    background-color: #fff;
+    padding: 15px;
+`
+
+const Main = styled.View`
+    flex: 1;
+    margin-top: 50px;
+`
+
+const AvatarContainer = styled.View`
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+`
+
+const Avatar = styled.Image`
+    width: 136;
+    height: 136;
+    border-radius: 68px;
+`
+
+const ButtonsContainer = styled.View`
+    justify-content: center;
+    align-items: center;
+    padding: 10px 30px;
+`
